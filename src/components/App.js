f702ad59085aa296936a17ede9237fb10d6b0bbe
shoaib <sho.akh99941@gmail.com>
2023-07-07 17:28:41 +0530

    relative list

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,12 @@ class App extends Component {
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol key={"relativeList"}>
+                <li key={"relativeListItem1"}>shoaib</li>
+                <li key={"relativeListItem2"}>Abdul</li>
+                <li key={"relativeListItem3"}>namit</li>
+                <li key={"relativeListItem4"}>mushtaq</li>
+               </ol>
             </div>
         )
     }
